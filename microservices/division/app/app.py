814a7d2e861795ac4a7b5cf69bc7d1c9913382f8
Suraj <surajpes23460@gmail.com>
2023-04-18 14:40:52 +0530

    Final project

---
+++ microservices/division/app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+import requests
+import os
+
+app = Flask(__name__)
+app.secret_key = 'thisisjustarandomstring'
+
+@app.route("/<int:number_1>/<int:number_2>", methods=['POST', 'GET'])
+def divide(number_1,number_2):
+    try:
+        result = float(number_1) / float(number_2)
+    except ZeroDivisionError:
+        return "Error: division by zero"
+    else:
+        return str(result)
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=5050,
+        host="0.0.0.0"
+    )
---
+++ microservices/lcm/app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+import requests
+import os
+
+app = Flask(__name__)
+app.secret_key = 'thisisjustarandomstring'
+
+@app.route("/<int:number_1>/<int:number_2>", methods=['POST', 'GET'])
+def lcm(number_1,number_2):
+    if number_2 == 0:
+        return "Cannot find LCM of zero."
+    grt = max(number_1,number_2)
+    les = min(number_1,number_2)
+    temp = grt
+    while(True):
+        if(temp%les == 0):
+            return str(temp)
+        temp += grt
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=5050,
+        host="0.0.0.0"
+    )
